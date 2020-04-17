9117df6c89e6398095bc8491721362703eb5a680
alemucci <alemucci@tiscali.it>
2020-04-17 18:24:08 +0200

    Added Supplier entity

---
+++ src/main/java/it/contrader/view/SupplierView.java
@@ -0,0 +1,51 @@
+package it.contrader.view;
+
+import java.util.List;
+
+import it.contrader.controller.Request;
+import it.contrader.dto.SupplierDTO;
+import it.contrader.main.MainDispatcher;
+
+public class SupplierView extends AbstractView {
+
+	private Request request;
+	private String choice;
+
+	public SupplierView() {
+		
+	}
+
+	// Mostra la lista
+	@Override
+	public void showResults(Request request) {
+		if (request != null) {
+			System.out.println("\n------------------- Gestione fornitori -------------\n");
+			System.out.println("ID\tNome\tPaese\tEmail");
+			System.out.println("----------------------------------------------------\n");
+			
+			@SuppressWarnings("unchecked")
+			List<SupplierDTO> suppliers = (List<SupplierDTO>) request.get("suppliers");
+			for (SupplierDTO u: suppliers)
+				System.out.println(u);
+			System.out.println();
+		}
+	}
+
+	// Chiede all'utente un input (lettera da tastiera) per la choice (vedi Controller). 
+	// Mette la modalità GETCHOICE nella mode.
+	@Override
+	public void showOptions() {
+		System.out.println("          Scegli l'operazione da effettuare:");
+		System.out.println("[L]eggi [I]nserisci [M]odifica [C]ancella [B]ack [E]sci");
+		this.choice = getInput();
+	}
+	
+	// Impacchetta la request e la manda al Controller.
+	@Override
+	public void submit() {
+		request = new Request();
+		request.put("choice", choice);
+		request.put("mode", "GETCHOICE");
+		MainDispatcher.getInstance().callAction("Supplier", "doControl", this.request);
+	}
+}
